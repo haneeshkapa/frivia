ea0a664092f646d666368b411def1dc74994d55a
haneeshkapa <haneeshkapa@gmail.com>
2022-06-02 08:09:40 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:frivia/pages/game_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Frivia',
+      theme: ThemeData(
+        fontFamily: 'Roboto',
+        scaffoldBackgroundColor: const Color.fromRGBO(31, 31, 31, 1.0),
+        primarySwatch: Colors.blue,
+      ),
+      home: GamePage(),
+    );
+  }
+}
---
+++ lib/pages/game_page.dart
@@ -0,0 +1,64 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class GamePage extends StatelessWidget {
+  double? _deviceHeight, _deviceWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return _buildUI();
+  }
+
+  Widget _buildUI() {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: _deviceHeight! * 0.05),
+          child: _gameUI(),
+        ),
+      ),
+    );
+  }
+
+  Widget _gameUI() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        _questionText(),
+        _trueButton(),
+      ],
+    );
+  }
+
+  Widget _questionText() {
+    return const Text(
+      "test question 1 nothing interesting",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 25,
+        fontWeight: FontWeight.w300,
+      ),
+    );
+  }
+
+  Widget _trueButton() {
+    return MaterialButton(
+      onPressed: () {},
+      color: Colors.green,
+      minWidth: _deviceWidth! * 0.8,
+      height: _deviceHeight! * 0.10,
+      child: const Text(
+        "True",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+        ),
+      ),
+    );
+  }
+}
